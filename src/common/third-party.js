bae481c74bad340cf27f428f6c25c171d7f557f2
PostPCEra <vision21c@gmail.com>
2019-07-24 08:50:34 -0700

    v 0.1

---
+++ src/common/third-party.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = {
+  cosmiconfig: require("cosmiconfig"),
+  findParentDir: require("find-parent-dir").sync,
+  getStream: require("get-stream"),
+  isCI: () => require("is-ci"),
+  writeFileAtomic: require("write-file-atomic").sync
+};
